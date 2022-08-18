07999577676cdd42a2124374c18b7db9ab4ee34c
Sibylle Blondeau <sibylle.blondeau@hotmail.fr>
2022-08-18 11:07:04 +0200

    frontend nuxt

---
+++ frontend/groupomania/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <!DOCTYPE html>
+  <html lang="fr">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </head>
+
+    <body>
+      <div id="app">
+        <h1>Bienvenue</h1>
+        <NuxtLink to="/inscription">Je m'inscris</NuxtLink>
+        <NuxtLink to="/profil">Mon profil</NuxtLink>
+      </div>
+    </body>
+  </html>
+</template>
