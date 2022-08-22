41e7e79aae6369a70869941d24756cf1489349b5
Sibylle Blondeau <sibylle.blondeau@hotmail.fr>
2022-08-22 11:48:52 +0200

    premier compo

---
+++ frontend/groupomania/components/post.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <p>{{ content }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "publication",
+  props: ["title", "content"],
+};
+</script>
--- frontend/groupomania/pages/index.vue
+++ frontend/groupomania/pages/index.vue
@@ -1,17 +1,24 @@
 <template>
   <!DOCTYPE html>
   <html lang="fr">
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </head>
-
     <body>
       <div id="app">
         <h1>Bienvenue</h1>
         <NuxtLink to="/inscription">Je m'inscris</NuxtLink>
         <NuxtLink to="/profil">Mon profil</NuxtLink>
       </div>
+
+      <publication title="ma publication" content="bonjour vous" />
     </body>
   </html>
 </template>
+
+<script>
+import publication from "~/components/publication";
+
+export default {
+  components: {
+    publication,
+  },
+};
+</script>
